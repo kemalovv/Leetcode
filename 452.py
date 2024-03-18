3bd217d35b9512fa274471f653c8a49a56595675
Kemalov Daniyar <kemalov2011@mail.ru>
2024-03-18 23:52:41 +0300

    452

--- 452.py
+++ 452.py
@@ -1,4 +1,13 @@
 def find_min_arrow_shots(points):
-    ans = [points[0]]
+    points.sort()
+    ans = len(points)
+    prev = points[0]
+
     for i in range(1, len(points)):
-        if points[i][0] >
+        if points[i][0] <= prev[1]:
+            ans -= 1
+            prev = [points[i][0], min(points[i][1], prev[1])]
+        else:
+            prev = points[i]
+
+    return ans
